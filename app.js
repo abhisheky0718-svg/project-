970054766f9ce1907e461c1f816178cb64899287
abhishek <abhisheky0718@gmail.com>
2026-06-20 14:46:39 +0530

    project

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL ;
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 // Force Node DNS to use reliable public resolvers so SRV lookups work in restricted networks
 try {
